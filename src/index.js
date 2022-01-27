0abe05033fa054439e0f7cd8aee4cf966a73fb55
SerZatr <90468588+SerZatr@users.noreply.github.com>
2022-01-27 04:42:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+function updateSize() {
+  let height = window.innerHeight + 'px';
+  document.getElementById('root').style.setProperty('height', height);
+}
+
+window.onload = updateSize;
+window.onresize = updateSize;
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
